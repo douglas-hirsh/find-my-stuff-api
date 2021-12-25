9bdd80abc02e09cb948ee6f7cc32f37d67925012
Douglas Hirsh <dhirshjr@gmail.com>
2021-12-25 10:51:27 -0600

    feat:initial work

---
+++ core/config.py
@@ -0,0 +1,5 @@
+class Settings:
+    PROJECT_TITLE: str = "Stuff API"
+    PROJECT_VERSION: str = "0.1.0"
+
+settings = Settings()
--- main.py
+++ main.py
@@ -1,8 +1,61 @@
 from fastapi import FastAPI
+from typing import List
+from pydantic import BaseModel
 
-app = FastAPI()
+from core.config import settings
 
+app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
+
+class Location(BaseModel):
+    id: int
+    name: str
+    path: str
+    user_id: int
+
+locations: List[Location] = [Location(**{
+  "id": 0,
+  "name": "string",
+  "path": "string",
+  "user_id": 0
+})]
+
+current_id = 1
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/locations", response_model=List[Location])
+async def get_locations():
+    return locations
+
+@app.post("/locations", response_model=Location)
+async def create_location(location: Location):
+    global current_id 
+
+    location.id = current_id
+    current_id = current_id + 1
+
+    locations.append(location)
+
+    return location
+
+@app.put("/locations/{location_id}")
+async def update_locations(location_id: int, location: Location):
+    print(locations)
+    existing_location = next((location for location in locations if location.id == location_id), None)
+    print(existing_location)
+
+    if existing_location:
+        existing_location.dict().update(location.dict())
+        print(locations)
+        return existing_location
+    else:
+        return {"error": f"Location with id {location_id} not found."}, 404
+
+    
+
+@app.get("/locations/{id}")
+async def get_location(id: int):
+    return {"data": {"name": f"Location {id}", "id": id}}
+
