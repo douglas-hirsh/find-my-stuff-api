e23ebf35970a5ff8608fd2efb60caaaa0099ecf1
Douglas Hirsh <dhirshjr@gmail.com>
2021-12-25 11:49:01 -0600

    feat: set up database

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
 from typing import List
 from pydantic import BaseModel
+from db.session import engine
+from db.base_class import Base
 
 from core.config import settings
 
-app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
+def start_application():
+    app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
+    create_tables()
+    return app
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+app = start_application()
 
 class Location(BaseModel):
     id: int
