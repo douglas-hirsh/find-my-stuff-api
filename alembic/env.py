b4524af6c2edc831e8dfbfc0ba46375ef243403b
Douglas Hirsh <dhirshjr@gmail.com>
2022-01-01 14:52:01 -0600

    fix: remove print statement from alembic env.py

--- alembic/env.py
+++ alembic/env.py
@@ -64,7 +64,6 @@ def run_migrations_online():
     """
     configuration = config.get_section(config.config_ini_section)
     configuration["sqlalchemy.url"] = settings.DATABASE_URL
-    print(configuration)
     
     connectable = engine_from_config(
         configuration,
