4f155b44a1abc7ca95ffbf572c53a7a274a81d3c
Douglas Hirsh <dhirshjr@gmail.com>
2022-01-02 14:26:00 -0600

    refactor: remove create tables and unused usings from main.py

--- main.py
+++ main.py
@@ -1,29 +1,14 @@
 from fastapi import FastAPI, Request
-from typing import List, Optional
-from pydantic import BaseModel
-from db.models.users import User
-from db.repository.location import create_new_location
-from db.session import engine, get_db
-from db.base_class import Base
-from db.base import Base
+
 from api.version1 import route_users
 from api.version1 import route_login 
 from api.version1 import route_locations 
 from api.version1 import route_items 
-from sqlalchemy.orm import Session
-
 from core.config import settings
-
-from api.version1.route_login import get_current_user_from_token
-from schemas.location import CreateLocation, ShowLocation 
 
 def start_application():
     app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
-    create_tables()
     return app
-
-def create_tables():
-    Base.metadata.create_all(bind=engine)
 
 app = start_application()
 
