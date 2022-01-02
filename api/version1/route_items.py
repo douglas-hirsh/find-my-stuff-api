f36702ac27c7404ba3f2600d9e7f38bfdd7dd294
Douglas Hirsh <dhirshjr@gmail.com>
2022-01-02 13:50:56 -0600

    feat: add get items route

--- api/version1/route_items.py
+++ api/version1/route_items.py
@@ -12,6 +12,15 @@ from schemas.items import CreateItem, ShowItem
 
 router = APIRouter()
 
+@router.get('/', response_model=List[ShowItem])
+async def get_items(location_id, db: Session = Depends(get_db), current_user:User = Depends(get_current_user_from_token)):
+    location = get_location_by_id(location_id, db, current_user)
+
+    if not location:
+        raise HTTPException(status_code=404, detail="Location not found")
+
+    return location.items
+
 @router.post("/", response_model=ShowItem)
 async def create_item(location_id, item: CreateItem, db: Session = Depends(get_db), current_user:User = Depends(get_current_user_from_token)):
     location = get_location_by_id(location_id, db, current_user)
